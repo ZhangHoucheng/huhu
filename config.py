985bd955a8bf5a9c51424cd1d3c14b56800f2eee
zhc <zhc@localhost.localdomain>
2015-04-24 23:45:57 +0800

    config

--- config.py
+++ config.py
@@ -11,16 +11,8 @@ user = 'root'
 dbname = 'huhu'
 password = ''
 
-<<<<<<< HEAD
 if os.name == 'nt':
     upload_dir = os.getcwd()+"\\static\\image\\upload\\"
 else:
     upload_dir = os.getcwd()+"/static/image/upload/"
-=======
-if os.name =='nt':
-    upload_dir = os.getcwd()+"\\static\\image\\upload\\"
-else :
-    upload_dir = os.getcwd()+"/static/image/upload/"
 
-    
->>>>>>> dd7535a590786452e729b3f45f8ae8a6368b5b7b
