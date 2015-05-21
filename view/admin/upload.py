b1698d1fd8431970c5fc6a05bfe3dc3d0788bd5c
ZHC <ZHC@ZHCdeMacBook-Pro.local>
2015-05-21 10:39:02 +0800

    modified upload

--- view/admin/upload.py
+++ view/admin/upload.py
@@ -17,13 +17,13 @@ class UploadHandler(RequestHandler):
         type = self.get_argument('type')
         description = self.get_argument('desc')
         tag = self.get_argument('tag')
-        upload_path=os.path.join(upload_dir,type)  #文件的暂存路径
-        file_metas=self.request.files['file']    #提取表单中‘name’为‘file’的文件元数据
+        upload_path=os.path.join(upload_dir,type)
+        file_metas=self.request.files['file']
         for meta in file_metas:
             filename=meta['filename']
             inname = commonfunction.random_str(8)+filename
             filepath=os.path.join(upload_path,inname)
-            with open(filepath,'wb') as up:      #有些文件需要已二进制的形式存储，实际中可以更改
+            with open(filepath,'wb') as up:
                 up.write(meta['body'])
 #             innerpath = filepath.replace('\\' ,'|')
             db_connection.create_image(filename,type, inname, description, tag)
